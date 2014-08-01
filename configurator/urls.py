7e8059ab55d0eed1f80989b75482700e625953cf
Jaime Ibar <jim2k7@gmail.com>
2014-08-02 00:06:29 +0200

    Added imports needed for gunicorn

--- configurator/urls.py
+++ configurator/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,3 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/', include("nodes.urls", namespace="index")),
 )
+
+urlpatterns += staticfiles_urlpatterns()
