a5bc715f5c12a383d7f41a57336ef8effb00e3f3
Jaime Ibar <jim2k7@gmail.com>
2014-08-05 17:16:14 +0200

    Added some logging messages

--- nodes/views.py
+++ nodes/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.core import serializers
 import simplejson as json
 from pyghmi.ipmi import command
+from pyghmi.exceptions import IpmiException
 
 from nodes.models import Site, Node
 
@@ -27,6 +28,7 @@ def index(request):
             return HttpResponse(json_, content_type="application/json")
         elif 'selectedhosts' in request.GET.keys():
             data = request.GET.getlist('selectedhosts')
+            logger.debug('Data: {0}'.format(data))
             rescmd = request.GET.getlist('cmd').pop()
             execute_ipmi_command(data, rescmd)
             jsondata = json.dumps(RESULT)
@@ -37,18 +39,28 @@ def index(request):
 
 
 def do_command(result, ipmisession):
-    logger.info('foo')
+    host = ipmisession.bmc
+    logger.info('Executing session for {0}'.format(host))
     if 'error' in result:
-        print('Error for node {0}'.format(ipmisession.bmc))
+        logger.error('Error {0} in node {1}'.format(result.get('error'), host))
         return
     command_ = IPMICMD
+    logger.info('Command: {0}'.format(command_))
     if command_ == 'status':
-        value = ipmisession.get_power()
+        try:
+            logger.info('Executing command {0} in {1}'.format(command_, host))
+            value = ipmisession.get_power()
+        except IpmiException as e:
+            logger.error('Error executing command {0} in {1}: {2}'.format(command_, host, e))
+            return
     elif command_ == 'up':
+        logger.info('Executing command {0} in {1}'.format(command_, host))
         value = ipmisession.set_power('on', wait=True)
     elif command_ == 'down':
+        logger.info('Executing command {0} in {1}'.format(command_, host))
         value = ipmisession.set_power('off', wait=True)
-    RESULT[ipmisession.bmc] = {'power': value.get('powerstate')}
+    logger.info('Executing OK for {0}'.format(host))
+    RESULT[host] = {'power': value.get('powerstate')}
 
 
 def execute_ipmi_command(host_list, ipmicommand):
