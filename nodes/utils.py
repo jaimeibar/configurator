22a65a1b7fad32fda8e7c3ae40752a6588868994
Jaime Ibar <jim2k7@gmail.com>
2014-08-16 00:01:10 +0200

    File for utils

---
+++ nodes/utils.py
@@ -0,0 +1,13 @@
+from nodes.models import Node
+
+
+def get_ip_from_hostname(hostame):
+    hostn = hostame + '.aragrid.es'
+    ip = Node.objects.filter(hostname__exact=hostn).values_list('ip', flat=True).first()
+    return ip
+
+
+def get_hostname_from_ip(hostip):
+    hname = Node.objects.filter(ip__exact=hostip).values_list('hostname', flat=True).first()
+    shname = hname.split('.', 1)[0]
+    return shname
