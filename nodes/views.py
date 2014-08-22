3b3b3d8f4fa05f1f78fa90a3f523cd9265a1fab6
Jaime Ibar <jim2k7@gmail.com>
2014-08-22 16:30:48 +0200

    First version of using django session to store the task id

--- nodes/views.py
+++ nodes/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.core import serializers
 import simplejson as json
 from celery.exceptions import TimeoutError
+from celery.result import AsyncResult
 
 from nodes.models import Site, Node
 from nodes.tasks import execute_ipmi_command
@@ -30,25 +31,37 @@ def index(request):
             rescmd = request.GET.getlist('cmd').pop()
             logger.info('Command: {0}'.format(rescmd))
             res = execute_ipmi_command.apply_async((data, rescmd))
+            request.session['taskid'] = res.id
             logger.info('Session taskid: {0}'.format(res.id))
-            time.sleep(1)
             logger.info('Executing ipmi command')
             while True:
-                if res.state == 'SUCCESS':
+                taskstate = AsyncResult(request.session.get('taskid')).state
+                logger.info('Task {0} ----> Status: {1}'.format(request.session.get('taskid'), taskstate))
+                if taskstate == 'SUCCESS':
                     try:
                         outres = res.get()
+                        del request.session['taskid']
                         break
                     except TimeoutError as excp:
                         logger.error('Timeout error: {0}'.format(excp))
                         time.sleep(5)
+                elif taskstate == 'REVOKED':
+                    del request.session['taskid']
+                    outres = ''
+                    break
                 else:
+                    print('foo: {0}'.format(taskstate))
                     logger.info('Output not ready yet. Waiting 5 seconds')
                     time.sleep(5)
             jsondata = json.dumps(outres)
             logger.info('Json: {0}'.format(jsondata))
             return HttpResponse(jsondata, content_type='application/json')
         elif 'cancel' in request.GET:
-            logger.info('Cancelling tasks')
+            taskid = request.session.get('taskid')
+            logger.debug('Cancelling task id: {0}'.format(taskid))
+            # app.control.revoke(taskid, terminate=True, signal='KILL')
+            AsyncResult(taskid).revoke(terminate=True, signal='KILL')
+            logger.debug('Cancelled: {0}'.format(taskid))
             return HttpResponse(json.dumps({}), content_type='application/json')
     else:
         sites = Site.objects.all()
