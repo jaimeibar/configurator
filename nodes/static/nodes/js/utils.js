3cfb9e492a1c82ff0862ecb90dff6434210253dc
Jaime Ibar <jim2k7@gmail.com>
2014-10-27 13:58:25 +0100

    Fixed errors in jquery selectors variables. Forgot "#" in selector :-(

--- nodes/static/nodes/js/utils.js
+++ nodes/static/nodes/js/utils.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
     interval = null;
     bifibutton = $("#Bifi");
     cienciasbutton = $("#Ciencias");
-    epshbutton = $("EPSH");
+    epshbutton = $("#EPSH");
     euptbutton = $("#EUPT");
-    allbutton = $("all");
-    clearbutton = $("clear");
+    allbutton = $("#all");
+    clearbutton = $("#clear");
     commands = $("#commands");
     gobutton = $("#go");
-    stopbutton = $("stop");
+    stopbutton = $("#stop");
 });
 
 function get_selected(sname) {
@@ -136,7 +136,7 @@ function manage_go_button() {
 }
 
 function manage_clear_button(state) {
-    clearbutton.prop("disabled", state);
+    clearbutton.prop("disabled", false);
 }
 
 function checked_host() {
