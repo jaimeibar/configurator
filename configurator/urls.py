421f963592cb17195818b68ad9a2051f581629e0
Jaime Ibar <jim2k7@gmail.com>
2014-11-12 10:06:32 +0100

    Added url for login screen

--- configurator/urls.py
+++ configurator/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'nodes/login.html'}),
     url(r'^index/', include("nodes.urls", namespace="index")),
 )
